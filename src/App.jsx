cfc74571dec74f5201629c6fb9f9d714eeb2106b
nhatminh0509 <minhnhat@bacoor.co>
2021-12-31 11:14:47 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import React, { useContext } from 'react';
-import './App.scss';
-import Header from './Components/Header';
-import './Styles/override.less'
-import './Styles/global.scss'
-import AppContext from './Context/AppContext';
-import { createGlobalStyle } from 'styled-components'
-import Automatic from './Pages/Automatic';
+// import './App.scss';
+// import Header from './Components/Header';
+// import './Styles/override.less'
+// import './Styles/global.scss'
+// import AppContext from './Context/AppContext';
+// import { createGlobalStyle } from 'styled-components'
+// import Automatic from './Pages/Automatic';
 
 function App() {
-  const [app] = useContext(AppContext)
-  const { isDarkMode } = app
-  console.log(isDarkMode)
-  const GlobalStyles = createGlobalStyle`
-  html{
-    --background: ${isDarkMode ? 'rgb(33, 36, 41)' : 'rgb(247, 248, 250)'};
-    --background-mask: ${isDarkMode ? 'radial-gradient(150% 100% at 50% 0%, rgb(10, 41, 75) 0%, rgb(34, 30, 48) 50%, rgb(31, 33, 40) 100%)': 'radial-gradient(150% 100% at 50% 0%, rgb(205, 232, 251) 0%, rgb(252, 243, 249) 50%, rgb(255, 255, 255) 100%)'};
-    --background-color: ${isDarkMode ? 'rgb(33, 36, 41)' : '#fff'};
-    --background-color2: ${isDarkMode ? 'rgb(67, 71, 76)' : '#f2f2f2'};
-    --color: ${isDarkMode ? 'rgb(195, 197, 203)' : 'rgb(86, 90, 105)'};
-    --color-active: ${isDarkMode ? '#fff' : '#333'};
-    --background-button: ${isDarkMode ? 'linear-gradient(to right, #614385 , #516395)' : 'linear-gradient(to right, #2193b0 , #6dd5ed)'}
-  }
-  `
+  // const [app] = useContext(AppContext)
+  // const { isDarkMode } = app
+  // console.log(isDarkMode)
+  // const GlobalStyles = createGlobalStyle`
+  // html{
+  //   --background: ${isDarkMode ? 'rgb(33, 36, 41)' : 'rgb(247, 248, 250)'};
+  //   --background-mask: ${isDarkMode ? 'radial-gradient(150% 100% at 50% 0%, rgb(10, 41, 75) 0%, rgb(34, 30, 48) 50%, rgb(31, 33, 40) 100%)': 'radial-gradient(150% 100% at 50% 0%, rgb(205, 232, 251) 0%, rgb(252, 243, 249) 50%, rgb(255, 255, 255) 100%)'};
+  //   --background-color: ${isDarkMode ? 'rgb(33, 36, 41)' : '#fff'};
+  //   --background-color2: ${isDarkMode ? 'rgb(67, 71, 76)' : '#f2f2f2'};
+  //   --color: ${isDarkMode ? 'rgb(195, 197, 203)' : 'rgb(86, 90, 105)'};
+  //   --color-active: ${isDarkMode ? '#fff' : '#333'};
+  //   --background-button: ${isDarkMode ? 'linear-gradient(to right, #614385 , #516395)' : 'linear-gradient(to right, #2193b0 , #6dd5ed)'}
+  // }
+  // `
   return (
     <>
-      <Header/>
+    123
+      {/* <Header/>
       <div className='base-container'>
         <Automatic />
       </div>
-      <GlobalStyles />
+      <GlobalStyles /> */}
     </>
   );
 }
